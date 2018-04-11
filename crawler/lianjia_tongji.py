7d1ed334b6d5b8f2ffaa07c492747d73c2c7755c
牛明珠 <niumz@cmgos.com>
2018-04-11 09:49:25 +0800

    Add district crawl

--- crawler/lianjia_tongji.py
+++ crawler/lianjia_tongji.py
@@ -11,14 +11,15 @@ uri = 'mongodb://mzniu:Nmz53187223@www.mzniu.com:27017'
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
 tongji_url = 'https://bj.lianjia.com/fangjia/'
-list_url = 'http://210.75.213.188/shh/portal/bjjs2016/list.aspx?pagenumber='
-detailed_url = 'http://210.75.213.188/shh/portal/bjjs2016/'
+area_tup = (
+    "dongcheng", "xicheng","chaoyang", "haidian", "fengtai", "shijingshan", "tongzhou", "changping", "daxing",
+    "shunyi", "fangshan", "mentougou", "pinggu", "huairou", "miyun", "yanqing")
 
 
 def get_record():
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
-    print "当前需要添加日期："+str(yesterday.strftime(str_format))
+    print "当前需要添加日期：" + str(yesterday.strftime(str_format))
     response = requests.get(tongji_url)
     response.encoding = response.apparent_encoding
     if response.status_code == 200:
@@ -38,15 +39,28 @@ def get_record():
                 lianjia_average_num = soup.find('div', class_="qushi-2").find('span').text
                 new_publish = soup.find_all('div', class_="item item-1-3")[0].find_all('span')[2].text
                 new_buyer = soup.find_all('div', class_="item item-1-3")[1].find('div', class_="num").find('span').text
-                new_takelook = soup.find_all('div', class_="item item-1-3")[2].find('div', class_="num").find('span').text
+                new_takelook = soup.find_all('div', class_="item item-1-3")[2].find('div', class_="num").find(
+                    'span').text
                 on_sale = \
-                    soup.find('div', class_="qushi-2").find_all('a', class_="txt")[0].text.split(u"房源")[1].split(u"套")[0]
+                    soup.find('div', class_="qushi-2").find_all('a', class_="txt")[0].text.split(u"房源")[1].split(u"套")[
+                        0]
                 recent90_trans = \
-                soup.find('div', class_="qushi-2").find_all('a', class_="txt")[1].text.split(u"房源")[1].split(u"套")[0]
-                result = {"update_date": str(datetime.datetime.today()),"date":str(yesterday.strftime(str_format)),
-                          lianjia_average_title:lianjia_average_num,"new_publish":new_publish,"new_buyer":new_buyer,
-                          "new_takelook":new_takelook,"on_sale":on_sale,"recent90_trans":recent90_trans}
-                print lianjia_average_num,new_publish, new_buyer, new_takelook, on_sale, recent90_trans
+                    soup.find('div', class_="qushi-2").find_all('a', class_="txt")[1].text.split(u"房源")[1].split(u"套")[
+                        0]
+                result = {"update_date": str(datetime.datetime.today()), "date": str(yesterday.strftime(str_format)),
+                          lianjia_average_title: lianjia_average_num, "new_publish": new_publish,
+                          "new_buyer": new_buyer,
+                          "new_takelook": new_takelook, "on_sale": on_sale, "recent90_trans": recent90_trans}
+                print lianjia_average_num, new_publish, new_buyer, new_takelook, on_sale, recent90_trans
+                count_trans = 0
+                for item in area_tup:
+                    temp_result = get_area_record(item)
+                    result[item] = temp_result
+                    if temp_result["new_trans"] !=u"暂无数据":
+                        count_trans+= int(temp_result["new_trans"])
+                result["total_trans"]=count_trans
+                #print dict(result, **{table_data2[0][0]: json.loads(json.dumps(dict(table_data2[1:])))})
+
                 print "添加新数据到数据库！"
                 try:
                     client = MongoClient(uri)
@@ -59,6 +73,33 @@ def get_record():
                     house = client.aihouse
                     stat = house.lianjia_statistics
                     stat.insert(result)
+
+
+def get_area_record(area):
+    print "添加"+area+"数据："
+    response = requests.get(tongji_url + "/" + area)
+    response.encoding = response.apparent_encoding
+    if response.status_code == 200:
+        source = response.text
+    else:
+        print response.status_code
+    soup = BeautifulSoup(response.text, "html.parser")
+    if soup is not None:
+        lianjia_average_title = soup.find('div', class_="qushi-1").find('span').text
+        lianjia_average_num = soup.find('div', class_="qushi-2").find('span').text
+        new_trans = soup.find_all('div', class_="item item-1-2")[0].find('div', class_="num").find('span').text
+        new_takelook = soup.find_all('div', class_="item item-1-2")[1].find('div', class_="num").find(
+            'span').text
+        on_sale = \
+            soup.find('div', class_="qushi-2").find_all('a', class_="txt")[0].text.split(u"房源")[1].split(u"套")[
+                0]
+        recent90_trans = \
+            soup.find('div', class_="qushi-2").find_all('a', class_="txt")[1].text.split(u"房源")[1].split(u"套")[
+                0]
+        result = {lianjia_average_title: lianjia_average_num, "new_trans": new_trans,
+                  "new_takelook": new_takelook, "on_sale": on_sale, "recent90_trans": recent90_trans}
+        print lianjia_average_num, new_trans, new_takelook, on_sale, recent90_trans
+        return result
 
 
 def check_record():
@@ -77,4 +118,4 @@ def check_record():
 
 
 get_record()
-
+#get_area_record("tongzhou")
